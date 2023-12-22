6d14401b07f3b8e9cec821c19833c39b6cefa60c
Jesse Braham <jesse@beta7.io>
2023-12-22 06:05:58 -0800

    Move `Transport` type into library, extract some functions

--- src/lib.rs
+++ src/lib.rs
@@ -41,6 +41,11 @@ pub mod hal {
     pub use esp32s3_hal::*;
 }
 
+use self::{
+    hal::{Uart, peripherals::UART0},
+    io::Noop,
+};
+
 #[derive(Debug)]
 pub enum TransportMethod {
     Uart,
@@ -50,37 +55,62 @@ pub enum TransportMethod {
     UsbOtg,
 }
 
-pub fn detect_transport() -> TransportMethod {
-    #[cfg(usb0)]
-    use crate::targets::EspUsbOtgId as _;
-    #[cfg(usb_device)]
-    use crate::targets::EspUsbSerialJtagId as _;
-
-    #[repr(C)]
-    struct Uart {
-        baud_rate: u32,
-        data_bits: u32,
-        exist_parity: u32,
-        parity: u32,
-        stop_bits: u32,
-        flow_ctrl: u32,
-        buff_uart_no: u8,
-        rcv_buff: [u32; 2], // PAD
-        rcv_state: u32,
-        received: u32,
-    }
-
-    extern "C" {
-        fn esp_flasher_rom_get_uart() -> *const Uart;
-    }
-
-    let device = unsafe { esp_flasher_rom_get_uart() };
-    let num = unsafe { (*device).buff_uart_no };
-    match num {
-        #[cfg(usb_device)]
-        Target::USB_SERIAL_JTAG_ID => TransportMethod::UsbSerialJtag,
+impl TransportMethod {
+    pub fn detect() -> Self {
         #[cfg(usb0)]
-        Target::USB_OTG_ID => TransportMethod::UsbOtg,
-        _ => TransportMethod::Uart,
+        use crate::targets::EspUsbOtgId as _;
+        #[cfg(usb_device)]
+        use crate::targets::EspUsbSerialJtagId as _;
+
+        extern "C" {
+            fn esp_flasher_rom_get_uart() -> *const Uart;
+        }
+
+        #[repr(C)]
+        struct Uart {
+            baud_rate: u32,
+            data_bits: u32,
+            exist_parity: u32,
+            parity: u32,
+            stop_bits: u32,
+            flow_ctrl: u32,
+            buff_uart_no: u8,
+            rcv_buff: [u32; 2], // PAD
+            rcv_state: u32,
+            received: u32,
+        }
+
+        let device = unsafe { esp_flasher_rom_get_uart() };
+        let num = unsafe { (*device).buff_uart_no };
+
+        match num {
+            #[cfg(usb_device)]
+            Target::USB_SERIAL_JTAG_ID => TransportMethod::UsbSerialJtag,
+            #[cfg(usb0)]
+            Target::USB_OTG_ID => TransportMethod::UsbOtg,
+            _ => TransportMethod::Uart,
+        }
     }
 }
+
+// TODO this sucks, but default generic parameters are not used when inference
+// fails, meaning that we _have_ to specifiy the types here Seems like work on this has stalled: https://github.com/rust-lang/rust/issues/27336, note that I tried the feature and it didn't work.
+#[cfg(not(any(usb_device, usb0)))]
+pub type Transport = io::Transport<&'static mut Uart<'static, UART0>, Noop, Noop>;
+
+#[cfg(all(usb_device, not(usb0)))]
+pub type Transport = io::Transport<
+    &'static mut Uart<'static, UART0>,
+    &'static mut crate::hal::UsbSerialJtag<'static>,
+    Noop,
+>;
+
+#[cfg(all(not(usb_device), usb0))]
+pub type Transport = io::Transport<&'static mut Uart<'static, UART0>, Noop, Noop>; // TODO replace Noop with usb type later
+
+#[cfg(all(usb_device, usb0))]
+pub type Transport = io::Transport<
+    &'static mut Uart<'static, UART0>,
+    &'static mut crate::hal::UsbSerialJtag<'static>,
+    Noop, // TODO replace Noop with usb type later
+>;
