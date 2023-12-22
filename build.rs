95924c04ab3fb4409d98f18247ad73d4e05dc1ad
Jesse Braham <jesse@beta7.io>
2023-12-22 05:45:41 -0800

    Reduce code duplication in `build.rs`, define some configuration symbols

--- build.rs
+++ build.rs
@@ -1,106 +1,73 @@
-use std::{env, fs, path::PathBuf};
+use std::{env, error::Error, fs, path::PathBuf};
 
-fn main() {
-    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
-    println!("cargo:rustc-link-search)={}", out_dir.display());
+fn main() -> Result<(), Box<dyn Error>> {
+    let out = PathBuf::from(env::var("OUT_DIR").unwrap());
+    println!("cargo:rustc-link-search)={}", out.display());
 
-    #[cfg(feature = "esp32")]
+    let chip = if cfg!(feature = "esp32") {
+        "esp32"
+    } else if cfg!(feature = "esp32c2") {
+        "esp32c2"
+    } else if cfg!(feature = "esp32c3") {
+        "esp32c3"
+    } else if cfg!(feature = "esp32c6") {
+        "esp32c6"
+    } else if cfg!(feature = "esp32h2") {
+        "esp32h2"
+    } else if cfg!(feature = "esp32s2") {
+        "esp32s2"
+    } else if cfg!(feature = "esp32s3") {
+        "esp32s3"
+    } else {
+        panic!("Must select exactly one chip feature!")
+    };
+
+    let arch = match chip {
+        "esp32" | "esp32s2" | "esp32s3" => "xtensa",
+        _ => "riscv32",
+    };
+
+    // Define configuration symbols:
+
+    println!("cargo:rustc-cfg={chip}");
+    println!("cargo:rustc-cfg={arch}");
+
+    // Define any USB-related configuration symbols, if required:
+
+    if cfg!(feature = "esp32c3")
+        || cfg!(feature = "esp32c6")
+        || cfg!(feature = "esp32h2")
+        || cfg!(feature = "esp32s3")
     {
-        fs::copy("ld/esp32_stub.x", out_dir.join("esp32_stub.x")).unwrap();
-        println!("cargo:rerun-if-changed=ld/esp32_stub.x");
-        println!("cargo:rustc-link-arg=-Tld/esp32_stub.x");
-
-        fs::copy("ld/esp32_rom.x", out_dir.join("esp32_rom.x")).unwrap();
-        println!("cargo:rerun-if-changed=ld/ld/esp32_rom.x");
-        println!("cargo:rustc-link-arg=-Tld/esp32_rom.x");
+        println!("cargo:rustc-cfg=usb_device");
     }
 
-    #[cfg(feature = "esp32c2")]
-    {
-        fs::copy("ld/esp32c2_stub.x", out_dir.join("esp32c2_stub.x")).unwrap();
-        println!("cargo:rerun-if-changed=ld/esp32c2_stub.x");
-        println!("cargo:rustc-link-arg=-Tld/esp32c2_stub.x");
+    if cfg!(feature = "esp32s2") || cfg!(feature = "esp32s3") {
+        println!("cargo:rustc-cfg=usb0");
+    }
 
-        fs::copy("ld/esp32c2_rom.x", out_dir.join("esp32c2_rom.x")).unwrap();
-        println!("cargo:rerun-if-changed=ld/ld/esp32c2_rom.x");
-        println!("cargo:rustc-link-arg=-Tld/esp32c2_rom.x");
+    // Copy required linker scripts to the `out` path:
 
+    let ld_path = PathBuf::from("ld");
+
+    let stub_x = format!("{}_stub.x", chip);
+    fs::copy(ld_path.join(&stub_x), out.join(&stub_x))?;
+    println!("cargo:rerun-if-changed=ld/{stub_x}");
+    println!("cargo:rustc-link-arg=-Tld/{stub_x}");
+
+    let rom_x = format!("{}_rom.x", chip);
+    fs::copy(ld_path.join(&rom_x), out.join(&rom_x))?;
+    println!("cargo:rerun-if-changed=ld/{rom_x}");
+    println!("cargo:rustc-link-arg=-Tld/{rom_x}");
+
+    // The RISC-V devices additionally require the `hal-defaults.x` linker
+    // script from `esp-hal`, to avoid interrupt-related linker errors:
+
+    if arch == "riscv32" {
         println!("cargo:rustc-link-arg=-Thal-defaults.x");
     }
 
-    #[cfg(feature = "esp32c3")]
-    {
-        fs::copy("ld/esp32c3_stub.x", out_dir.join("esp32c3_stub.x")).unwrap();
-        println!("cargo:rerun-if-changed=ld/esp32c3_stub.x");
-        println!("cargo:rustc-link-arg=-Tld/esp32c3_stub.x");
+    // Done!
 
-        fs::copy("ld/esp32c3_rom.x", out_dir.join("esp32c3_rom.x")).unwrap();
-        println!("cargo:rerun-if-changed=ld/ld/esp32c3_rom.x");
-        println!("cargo:rustc-link-arg=-Tld/esp32c3_rom.x");
-
-        println!("cargo:rustc-link-arg=-Thal-defaults.x");
-    }
-
-    #[cfg(feature = "esp32c6")]
-    {
-        fs::copy("ld/esp32c6_stub.x", out_dir.join("esp32c6_stub.x")).unwrap();
-        println!("cargo:rerun-if-changed=ld/esp32c6_stub.x");
-        println!("cargo:rustc-link-arg=-Tld/esp32c6_stub.x");
-
-        fs::copy("ld/esp32c6_rom.x", out_dir.join("esp32c6_rom.x")).unwrap();
-        println!("cargo:rerun-if-changed=ld/ld/esp32c6_rom.x");
-        println!("cargo:rustc-link-arg=-Tld/esp32c6_rom.x");
-
-        println!("cargo:rustc-link-arg=-Thal-defaults.x");
-    }
-
-    #[cfg(feature = "esp32h2")]
-    {
-        fs::copy("ld/esp32h2_stub.x", out_dir.join("esp32h2_stub.x")).unwrap();
-        println!("cargo:rerun-if-changed=ld/esp32h2_stub.x");
-        println!("cargo:rustc-link-arg=-Tld/esp32h2_stub.x");
-
-        fs::copy("ld/esp32h2_rom.x", out_dir.join("esp32h2_rom.x")).unwrap();
-        println!("cargo:rerun-if-changed=ld/ld/esp32h2_rom.x");
-        println!("cargo:rustc-link-arg=-Tld/esp32h2_rom.x");
-
-        println!("cargo:rustc-link-arg=-Thal-defaults.x");
-    }
-
-    #[cfg(feature = "esp32s2")]
-    {
-        fs::copy("ld/esp32s2_stub.x", out_dir.join("esp32s2_stub.x")).unwrap();
-        println!("cargo:rerun-if-changed=ld/esp32s2_stub.x");
-        println!("cargo:rustc-link-arg=-Tld/esp32s2_stub.x");
-
-        fs::copy("ld/esp32s2_rom.x", out_dir.join("esp32s2_rom.x")).unwrap();
-        println!("cargo:rerun-if-changed=ld/ld/esp32s2_rom.x");
-        println!("cargo:rustc-link-arg=-Tld/esp32s2_rom.x");
-    }
-
-    #[cfg(feature = "esp32s3")]
-    {
-        fs::copy("ld/esp32s3_stub.x", out_dir.join("esp32s3_stub.x")).unwrap();
-        println!("cargo:rerun-if-changed=ld/esp32s3_stub.x");
-        println!("cargo:rustc-link-arg=-Tld/esp32s3_stub.x");
-
-        fs::copy("ld/esp32s3_rom.x", out_dir.join("esp32s3_rom.x")).unwrap();
-        println!("cargo:rerun-if-changed=ld/ld/esp32s3_rom.x");
-        println!("cargo:rustc-link-arg=-Tld/esp32s3_rom.x");
-    }
-
-    emit_cfg();
-}
-
-fn emit_cfg() {
-    #[cfg(any(
-        feature = "esp32c3",
-        feature = "esp32c6",
-        feature = "esp32h2",
-        feature = "esp32s3",
-    ))]
-    println!("cargo:rustc-cfg=usb_device");
-
-    #[cfg(any(feature = "esp32s2", feature = "esp32s3"))]
-    println!("cargo:rustc-cfg=usb0");
+    Ok(())
 }
